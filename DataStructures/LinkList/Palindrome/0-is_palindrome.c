f2d2f7d2dcc4fa20779976ddc52f9a9eb5c396ea
Nelson Jimenez <jmnel_mar@hotmail.com>
2020-08-22 12:54:08 -0700

    adding binary utils

---
+++ DataStructures/LinkList/Palindrome/0-is_palindrome.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - check if a singly linked list is a palindrome
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 1 if it is a palindrome or 0 if it is not a palindrome
+*/
+int is_palindrome(listint_t **head)
+{
+
+}
+
+/**
+ * reverse_listint - function that reversed a list
+ * @head: first element of tha list
+ * Return: return a new list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	listint_t *prev = NULL;
+
+	while (*head != NULL)
+	{
+		*head = temp->next;
+		temp->next = prev;
+		prev = temp;
+		temp = *head;
+	}
+	*head = prev;
+	return (*head);
+}
+
